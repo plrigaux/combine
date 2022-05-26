e5a5f85cf4532c4b47207d594d0e9bce05f0530d
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-05-26 17:31:11 -0400

    ok

--- src/app/comb-table/comb-table.component.ts
+++ src/app/comb-table/comb-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { newArray } from '@angular/compiler/src/util';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-comb-table',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CombTableComponent implements OnInit {
 
-  totalBalls = 4
-  balls = 2
+  @Input()
+  total :string = "4"
+
+  @Input()
+  nbpicks : string = "2"
+
+  combinations: number[][] = []
 
   constructor() { }
 
@@ -20,23 +27,34 @@ export class CombTableComponent implements OnInit {
 
     let blabla = []
 
-    for (let i = 0; i < this.totalBalls; i++) {
 
-      let row = new Array(this.totalBalls)
-
-      blabla.push(row)
-    }
-
-    for (let combination of combinations2(this.totalBalls, this.balls)) { 
+    for (let combination of combinations2(+this.total, +this.nbpicks)) {
       console.log(combination);
     }
+
+    this.combinations = combinations2(+this.total, +this.nbpicks)
+
+    console.log(this.combinations)
+
   }
 
+  getRowElem(selected: number[]): string[] {
 
+    let row: string[] = new Array(+this.total)
+
+    for (let num of selected) {
+      row[num - 1] = num.toString()
+    }
+    return row
+  }
+
+  classFromVal(val: string): string {
+    return val ? "fill" : ""
+  }
 }
 
 
-function* combinations(n :number, k : number) : IterableIterator<number[]> { 
+function* combinations(n: number, k: number): IterableIterator<number[]> {
   if (k < 1) {
     yield [];
   } else {
@@ -45,20 +63,55 @@ function* combinations(n :number, k : number) : IterableIterator<number[]> {
         tail.push(i);
         yield tail;
       }
-    } 
+    }
   }
 }
 
-function
- combinations2(n :number, k : number) : IterableIterator<number[]> { 
-  if (k < 1) {
-    yield [];
-  } else {
-    for (let i = k; i <= n; i++) {
-      for (let tail of combinations(i - 1, k - 1)) {
-        tail.push(i);
-        yield tail;
-      }
-    } 
+
+
+function combinations2(n: number, k: number): number[][] {
+
+  let combList: number[][] = []
+  let maxes = getMaxes(n, k)
+  let all = getAllNum(n, k)
+  console.log("maxes", maxes)
+
+  console.log("all", all)
+
+  combinations2x(n, k, 0, new Array(k), combList)
+
+  return combList
+}
+
+function combinations2x(all: number, len: number, startPosition: number, result: number[], allResuls: number[][]) {
+  if (len == 0) {
+    //console.log(result)
+    allResuls.push([...result])
+    return;
   }
+
+  for (let i = startPosition; i <= all - len;) {
+    i++
+    result[result.length - len] = i;
+    combinations2x(all, len - 1, i, result, allResuls);
+  }
+}
+
+function getMaxes(n: number, k: number): number[] {
+  let maxes: number[] = []
+
+  for (let i = n - k; i < n;) {
+    maxes.push(++i)
+  }
+  return maxes
+}
+
+function getAllNum(n: number, k: number): number[] {
+  let maxes: number[] = []
+
+  for (let i = 1; i <= n; i++) {
+    maxes.push(i)
+  }
+  return maxes
+
 }
