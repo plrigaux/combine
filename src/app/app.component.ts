5f725a1f3b95e111c65c38e5824fd4110190d5b1
plrigaux <491172+plrigaux@users.noreply.github.com>
2022-10-28 10:23:28 -0400

    new hope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
-import { formatNumber } from '@angular/common';
-import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
-import { angle, angle2, angle3, angle3DEG, calculate, calculateOdds, Coordinate, getCombinationfromIndex, getOddsIndex, toDegrees } from './tools';
+import { formatNumber } from '@angular/common'
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core'
+import {
+  angle,
+  angle2,
+  angle3,
+  angle3DEG,
+  calculate,
+  calculateOdds,
+  Coordinate,
+  getCombinationfromIndex,
+  getOddsIndex,
+  toDegrees
+} from './tools'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
-
 export class AppComponent implements OnInit {
-
   //totalNbNumbers: any = 0
   _totalNbNumbers: number = 0
-  set totalNbNumbers(val: any) {
+  set totalNbNumbers (val: any) {
     this._totalNbNumbers = Number(val)
 
     for (const selectedNumber of this._selected) {
@@ -23,7 +31,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  get totalNbNumbers() {
+  get totalNbNumbers () {
     return this._totalNbNumbers
   }
 
@@ -31,47 +39,50 @@ export class AppComponent implements OnInit {
   oddsIndex: any = 0
   combinationOutput: number[] = []
 
-  constructor(@Inject(LOCALE_ID) public locale: string,) { }
+  constructor (@Inject(LOCALE_ID) public locale: string) {}
 
-  ngOnInit() {
-    this.loadData();
+  ngOnInit () {
+    this.loadData()
 
     this.combbase_on_index()
   }
 
   private _odds: number = 0
-  combination() {
+  combination () {
     this._odds = calculateOdds(this.totalNbNumbers, this.drawNbNumber)
     return formatNumber(this._odds, this.locale)
   }
 
-  combbase_on_index() {
-
+  combbase_on_index () {
     if (!this.oddsIndex) {
       this.combinationOutput = []
     }
 
-    let comb = getCombinationfromIndex(this.totalNbNumbers, this.drawNbNumber, this.oddsIndex)
+    let comb = getCombinationfromIndex(
+      this.totalNbNumbers,
+      this.drawNbNumber,
+      this.oddsIndex
+    )
 
     this.combinationOutput = comb
 
     this.saveData()
   }
 
-  onCombIndexChange($event: any) {
+  onCombIndexChange ($event: any) {
     this.combbase_on_index()
   }
 
-  random() {
-    this.oddsIndex = Math.floor(Math.random() * calculateOdds(this.totalNbNumbers, this.drawNbNumber))
+  random () {
+    this.oddsIndex = Math.floor(
+      Math.random() * calculateOdds(this.totalNbNumbers, this.drawNbNumber)
+    )
 
     this.combbase_on_index()
   }
 
-  saveData() {
-
+  saveData () {
     let obj: SaveData = {
-
       total: this.totalNbNumbers,
       draw: this.drawNbNumber,
       combIndex: this.oddsIndex,
@@ -83,37 +94,36 @@ export class AppComponent implements OnInit {
     localStorage.setItem('DATA', str)
   }
 
-  private loadData() {
-    let data = localStorage.getItem('DATA');
+  private loadData () {
+    let data = localStorage.getItem('DATA')
     if (data) {
-      let parsed = JSON.parse(data) as SaveData;
+      let parsed = JSON.parse(data) as SaveData
 
-      this.oddsIndex = parsed.combIndex;
-      this.totalNbNumbers = parsed.total;
-      this.drawNbNumber = parsed.draw;
+      this.oddsIndex = parsed.combIndex
+      this.totalNbNumbers = parsed.total
+      this.drawNbNumber = parsed.draw
       this._selected = new Set(parsed.selected)
     }
   }
 
-
   private _selected: Set<number> = new Set<number>()
   private _allGeneratedNumbers: number[] = []
 
-  getTotalNumbers(): number[] {
-
+  getTotalNumbers (): number[] {
     if (this._allGeneratedNumbers.length != this.totalNbNumbers) {
-
-      this._allGeneratedNumbers = Array.from({ length: this.totalNbNumbers }, (_, i) => i + 1)
+      this._allGeneratedNumbers = Array.from(
+        { length: this.totalNbNumbers },
+        (_, i) => i + 1
+      )
 
       this._selected = new Set(
         [...this._selected].filter(x => x > this.totalNbNumbers)
-      );
+      )
     }
     return this._allGeneratedNumbers
   }
 
-  clickBall(ball: number) {
-
+  clickBall (ball: number) {
     if (!this._selected.delete(ball)) {
       if (this._selected.size < this.drawNbNumber) {
         this._selected.add(ball)
@@ -121,89 +131,100 @@ export class AppComponent implements OnInit {
     }
   }
 
-  drawed(ball: number): string {
-    return this._selected.has(ball) ? "drawed" : ""
+  drawed (ball: number): string {
+    return this._selected.has(ball) ? 'drawed' : ''
   }
 
   private _oddsIndex: number = 0
 
-
-  private _isAllNumberdrawed(): boolean {
+  private _isAllNumberdrawed (): boolean {
     return this._selected.size == this.drawNbNumber
   }
 
-  showOddIndex(): string {
+  showOddIndex (): string {
     if (!this._isAllNumberdrawed()) {
-      return ""
+      return ''
     }
 
-    let selectedSortedArray: number[] = [...this._selected].sort((a, b) => a - b);
-    this._oddsIndex = getOddsIndex(selectedSortedArray, this.totalNbNumbers, this.drawNbNumber)
+    let selectedSortedArray: number[] = [...this._selected].sort(
+      (a, b) => a - b
+    )
+    this._oddsIndex = getOddsIndex(
+      selectedSortedArray,
+      this.totalNbNumbers,
+      this.drawNbNumber
+    )
     //console.log(s, this.totalNbNumbers, this.drawNbNumber)
     this.saveData()
 
     return formatNumber(this._oddsIndex, this.locale)
   }
 
-  previousDisable(): boolean {
+  previousDisable (): boolean {
     return !(this._isAllNumberdrawed() && this._oddsIndex > 1)
   }
 
-  nextDisable(): boolean {
+  nextDisable (): boolean {
     return !(this._isAllNumberdrawed() && this._oddsIndex < this._odds)
   }
 
-  previous() {
+  previous () {
     if (this._oddsIndex <= 1) {
       return
     }
 
     this._oddsIndex--
-    let comb = getCombinationfromIndex(this.totalNbNumbers, this.drawNbNumber, this._oddsIndex)
+    let comb = getCombinationfromIndex(
+      this.totalNbNumbers,
+      this.drawNbNumber,
+      this._oddsIndex
+    )
     this._selected = new Set(comb)
   }
 
-  next() {
+  next () {
     if (this._oddsIndex >= this._odds) {
       return
     }
     this._oddsIndex++
-    let comb = getCombinationfromIndex(this.totalNbNumbers, this.drawNbNumber, this._oddsIndex)
+    let comb = getCombinationfromIndex(
+      this.totalNbNumbers,
+      this.drawNbNumber,
+      this._oddsIndex
+    )
     this._selected = new Set(comb)
   }
 
-  private step = 10;
+  private step = 10
 
-  stepper(): Iterable<number> {
+  stepper (): Iterable<number> {
     //return new Stepper(this.totalNbNumbers - 1, this.step)
     return setRange(0, this.totalNbNumbers - 1, this.step)
   }
 
-  substepper(start: number): Iterable<number> {
+  substepper (start: number): Iterable<number> {
     let limit = Math.min(start + this.step, this.totalNbNumbers)
     //return new Stepper(limit, 1, start + 1)
     return setRange(start + 1, limit, 1)
   }
 
-  angleShow(): number {
+  angleShow (): number {
     return angle()
   }
 
-  angleShow2(): number {
+  angleShow2 (): number {
     return angle2()
   }
 
-  angleShow3(): number {
+  angleShow3 (): number {
     return angle3DEG()
   }
 
-  angleShowRAD(): number {
+  angleShowRAD (): number {
     return angle3()
   }
 
-
-  angleShowEx(): number {
-
+  angleShowEx (): number {
     const p1: Coordinate = {
       lat: 39.099912,
       long: -94.581213
@@ -218,7 +239,7 @@ export class AppComponent implements OnInit {
     return toDegrees(angle)
   }
 
-  angleShowExMy(): number {
+  angleShowExMy (): number {
     const p1: Coordinate = {
       lat: 45.45601525767227,
       long: -73.54996188669251
@@ -233,9 +254,7 @@ export class AppComponent implements OnInit {
     return toDegrees(angle)
   }
 
-
-
-  angleShowExMy3(): number {
+  angleShowExMy3 (): number[] {
     const p1: Coordinate = {
       lat: 45.45601525767227,
       long: -73.54996188669251
@@ -247,11 +266,13 @@ export class AppComponent implements OnInit {
     }
 
     const angle = calculate(p1, p2)
-    return - toDegrees(angle) + 90
+    const val1 = -toDegrees(angle)
+    const val2 = toDegrees(angle)
+    //return - toDegrees(angle) + 90
+    return [val1, -toDegrees(angle + Math.PI / 4), val2]
   }
 
-
-  angleShowExMyTheThisn(): number {
+  angleShowExMyTheThisn (): number[] {
     const p1: Coordinate = {
       lat: 45.45601525767227,
       long: -73.54996188669251
@@ -264,23 +285,41 @@ export class AppComponent implements OnInit {
 
     const angle = calculate(p1, p2)
     //return (- angle + Math.PI / 2) / (2 * Math.PI)
-    return ((- angle / (2 * Math.PI)) + 0.25)
+    //return ((- angle / (2 * Math.PI)) + 0.25)
+    const val1 = -toDegrees(angle)
+    const val12 = -toDegrees(angle + Math.PI / 4)
+    const val2 = toDegrees(angle)
+
+    //return [val1, -toDegrees(angle + Math.PI / 4), val2]
+
+    const v1 = -angle / (2 * Math.PI)
+    const v2 = -angle / (2 * Math.PI) + 0.25
+    const v3 = 2 * Math.PI + angle / (2 * Math.PI)
+    return [v1, v2, v3]
   }
 
-  myIndex(): number {
-    const val = this.angleShowExMyTheThisn() * calculateOdds(50, 7)
-    return Math.round(val);
+  myIndex (): number[] {
+    const odds = calculateOdds(50, 7)
+
+    const vv = this.angleShowExMyTheThisn().map (v => {
+
+      return Math.round(v * odds)
+    } )
+   
+    return vv
   }
 }
 
-
 interface SaveData {
-  total: number,
-  draw: number,
+  total: number
+  draw: number
   combIndex: number
   selected: number[]
 }
 
 const setRange = (start: number, stop: number, step: number): number[] => {
-  return Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+  return Array.from(
+    { length: (stop - start) / step + 1 },
+    (_, i) => start + i * step
+  )
 }
